179d34f738d320849ee23d77d6af6a7535014e9b
JacquelineMongas <76976468+JacquelineMongas@users.noreply.github.com>
2022-10-02 00:43:39 +0200

    commit
    This week's work

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const links = [
     {label: "Week 1 notes", url: "week1/index.html",},
     {label: "Week 2 notes", url:"week2/index.html",},
     {label: "week 2 group work", url:"week2/groupwork.html",},
+    {label: "week 3 notes", url:"week3/index.html",},
+    {label: "week 3 group work", url:"week3/groupwork3.html" ,},
+    {label: "Group Work", url:"week3/teamwork3.html",},
    
   ];
   
